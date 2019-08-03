0a35547da2da5a11923a90a202db4041630e8850
sandflame321 <simon.vandeputte.y9117@student.hogent.be>
2019-08-03 11:53:04 +0200

    Adding Database and start of configuration

--- FinancialTracker/FinancialTracker/Data/ApplicationDbContext.cs
+++ FinancialTracker/FinancialTracker/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FinancialTracker.Data.Mappers;
+using FinancialTracker.Models.Domain;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +10,17 @@ namespace FinancialTracker.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Checkpoint> Checkpoints { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CheckpointConfiguration());
         }
     }
 }
---
+++ FinancialTracker/FinancialTracker/Data/Mappers/CheckpointConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FinancialTracker.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialTracker.Data.Mappers
+{
+    public class CheckpointConfiguration : IEntityTypeConfiguration<Checkpoint>
+    {
+        public void Configure(EntityTypeBuilder<Checkpoint> builder)
+        {
+            builder.ToTable("Checkpoints");
+        }
+    }
+}
